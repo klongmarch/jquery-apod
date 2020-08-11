75a441481c35e572117654d7b3e3a6117be0176f
steve kang <klongmarch@yahoo.com>
2020-08-11 12:07:56 -0500

    everything works

--- dist/js/main.js
+++ dist/js/main.js
@@ -10872,59 +10872,72 @@ return jQuery;
 } );
 
 var apod = {
-    // Application Constructor
-    init: function() {
+  //Injects the results of the API call into the DOM
+  buildDOM: function(result) {
+    $("#apodTitle").text(result.title);
 
-        let date = "2013-06-06";
+    if(result.media_type === 'video') {
+      $("#apodImage").hide();
+      $("#apodVideo > iframe").attr("src", result.url).show();
+    }else{
+      $("#apodVideo").hide();
+      $("#apodImg").attr("src", result.url).attr('alt', result.title).show();
+    }
 
-        // let date = this.randomDate(new Date(1995, 5, 16), new Date());
-	var url = "https://api.nasa.gov/planetary/apod?api_key=NJgjtxDubyLCAKXTGAhjze7IguWwCs8Rl22O6Dil&date=" + date;
+    $("#apodCopyright").text("Copyright: " + result.copyright);
+    $("#apodDate").text("Date: " + result.date);
+    $("#apodDesc").text(result.explanation);
+  },
 
-        $.ajax({
-            url: url
-        }).done(function(result){
-        $("#apodTitle").text(result.title);
-        // $("#apodImg").attr("src", result.url).attr('alt', result.title);
+  //Executes an AJAX call to an API.
+  getRequest: function() {
+    let _this = this;
+    // let date = "2013-06-06";
 
-	//If the media type is video hide the image elements and display a video.
-	if(result.media_type === 'video') {
-	  $("#apodImg").hide();
-	  $("#apodVideo > iframe").attr("src", result.url).show();
-	} else {
-	  $("#apodVideo").hide();
-	  $("#apodImg").attr("src", result.url).attr('alt', result.title).show();
-	}
+    let date = this.randomDate(new Date(1995, 5, 16), new Date());
+    var url = "https://api.nasa.gov/planetary/apod?api_key=NJgjtxDubyLCAKXTGAhjze7IguWwCs8Rl22O6Dil&date=" + date;
+    $.ajax({
+        url: url
+    }).done(function(result){
+        _this.buildDOM(result);
+    }).fail(function(result){
+      console.log(result);
+    });
+  },
 
-        $("#apodCopyright").text("Copyright: " + result.copyright);
-        $("#apodDate").text("Date: " + date);
-        $("#apodDesc").text(result.explanation);
-         }).fail(function(result){
-          console.log(result);
-        });
-    },
+  // Initialization method.
+  init: function() {
+    this.getRequest();
+  },
 
 	//Create a random date
 	randomDate: function(start, end) {
-  //Randomize the date https://gist.github.com/miguelmota/5b67e03845d840c949c4
-  let date = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+    //Randomize the date https://gist.github.com/miguelmota/5b67e03845d840c949c4
+    let date = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
 
-  //Format the date
-  let d = date.getDate();
-  let m = date.getMonth() + 1; //In JS months start at 0
-  let y = date.getFullYear();
+    //Format the date
+    let d = date.getDate();
+    let m = date.getMonth() + 1; //In JS months start at 0
+    let y = date.getFullYear();
 
-  //Change the month and day strings so that they match the documented format.
-  if(m < 10){
-    m = '0'+m
-  }
+    //Change the month and day strings so that they match the documented format.
+    if(m < 10){
+      m = '0'+m
+    }
 
-  if(d < 10){
-    d = '0'+d
-  }
+    if(d < 10){
+      d = '0'+d
+    }
 
-  return `${y}-${m}-${d}`;
-},
+    return `${y}-${m}-${d}`;
+  },
 };
 
 apod.init();
 
+/* https://learn.jquery.com/using-jquery-core/document-ready/ */
+$(function() {
+  $('#btnRandApod').on('click',function(){
+    apod.getRequest();
+  });
+});
